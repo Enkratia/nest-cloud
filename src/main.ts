773f2ddff515769577208e6b342e79078c1d48cd
Enkratia <manofthetreasure@gmail.com>
2023-10-29 14:06:07 +0300

    m

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import * as express from 'express';
+import path = require('path');
+
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: false });
+
+  app.enableCors({ credentials: true, origin: true });
+  app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+  const config = new DocumentBuilder()
+    .setTitle('Cloud storage')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  await app.listen(7777);
+}
+bootstrap();
